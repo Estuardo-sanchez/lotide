5376130d1fda7bdfb7041fe84d0879a5458f6dfd
Estuardo <estuardo_101@hotmail.com>
2022-03-27 18:22:22 -0400

    assertEqual arrays

---
+++ assertArraysEqual.js
@@ -0,0 +1,29 @@
+const eqArrays = function(arrayOne, arrayTwo) {
+  let sameLength = arrayOne.length === arrayTwo.length;
+  let sameValues = true;
+  for(i = 0; i < arrayOne.length; i++) {
+    if(arrayOne[i] !== arrayTwo[i]) {
+      sameValues = false;
+    }
+  }
+
+  if(sameValues && sameLength){
+    // console.log('Passed');
+    return true;
+  } else {
+    //console.log('Failed');
+    return false;
+  }
+}
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅ Assertion passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+assertArraysEqual(["Lighthouse Labs"], ["Bootcamp"]);
+assertArraysEqual(["hello"], ["hello"]);
+assertArraysEqual(["yes"], ["no"]);
+assertArraysEqual(["shoe"], ["shoe"], ["shoe"]);
