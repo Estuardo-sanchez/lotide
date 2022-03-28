11055611b3da6a10527d7cd349f5f554633d23ae
Estuardo <estuardo_101@hotmail.com>
2022-03-28 18:40:29 -0400

    flatten arrays

---
+++ flatten.js
@@ -0,0 +1,40 @@
+const eqArrays = function(arrayOne, arrayTwo) {
+  let sameLength = arrayOne.length === arrayTwo.length;
+  let sameValues = true;
+  for(i = 0; i < arrayOne.length; i++) {
+    if(arrayOne[i] !== arrayTwo[i]) {
+      sameValues = false;
+    }
+  }
+
+  if(sameValues && sameLength){
+    return true;
+  } else {
+    return false;
+  }
+}
+
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅ Assertion passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const flatten = function(array) {
+  let newArray = [];
+  for(let element of array) {
+    if(Array.isArray(element)) {
+      for(let elementTwo of element) {
+        newArray.push(elementTwo);
+      }
+    } else {
+      newArray.push(element);
+    }
+  }
+  console.log(newArray);
+};
+
+flatten([1, 2, [3, 4], 5, [6]]);
