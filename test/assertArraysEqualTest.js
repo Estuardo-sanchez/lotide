d7d39a9e3a7f762567066513f725d5502ffe5940
Estuardo <estuardo_101@hotmail.com>
2022-04-14 19:06:17 -0400

    tests

---
+++ test/assertArraysEqualTest.js
@@ -0,0 +1,8 @@
+const eqArrays = require('../eqArrays');
+const assertArraysEqual = require('../assertArraysEqual');
+
+
+assertArraysEqual(["Lighthouse Labs"], ["Bootcamp"]);
+assertArraysEqual(["hello"], ["hello"]);
+assertArraysEqual(["yes"], ["no"]);
+assertArraysEqual(["shoe"], ["shoe"], ["shoe"]);
