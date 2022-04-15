41e8c180d3fed195cc922c3a2a7140c96fce32cc
Estuardo <estuardo_101@hotmail.com>
2022-04-15 13:37:14 -0400

    refactored

--- tail.js
+++ tail.js
@@ -2,7 +2,7 @@ const assertEqual = require('./assertEqual');
 
 const tail = function(array) {
   let result = array.slice(1);
-  console.log(result);
+  return result;
 };
 
 module.exports = tail;
