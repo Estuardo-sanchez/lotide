93f2ad35d96a31b1173c6485d90f348fa1ccd4bb
Estuardo <estuardo_101@hotmail.com>
2022-04-15 13:41:21 -0400

    refactored

--- test/middleTest.js
+++ test/middleTest.js
@@ -3,14 +3,6 @@ const assertArraysEqual = require('../assertArraysEqual');
 const middle = require('../middle');
 const assert = require('chai').assert;
 
-
-// console.log(middle([1]));
-// console.log(middle([1, 2]));
-// console.log(middle([1, 2, 3]));
-// console.log(middle([1, 2, 3, 4, 5]));
-// console.log(middle([1, 2, 3, 4]));
-// console.log(middle([1, 2, 3, 4, 5, 6]));
-
  describe("#middle", () => {
    it("returns 3 for [1, 2, 3, 4, 5]", () => {
      assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]);
